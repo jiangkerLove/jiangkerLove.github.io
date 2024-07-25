022aa732b3ce05907e5701a2a2651a2562150a0e
auto_update <niklaus1996@icloud.com>
2024-07-25 09:33:41 +0000

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240725.172549';
+const cacheName = 'chirpy-20240725.173230';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
